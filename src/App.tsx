3b9b3a6fe2e776d92a46e97a91aaace1e42578b2
canlife4ever <canlife4ever@proton.me>
2025-01-08 12:37:42 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import Practice from "./pages/Practice";
+import Chat from "./pages/Chat";
 
 const queryClient = new QueryClient();
 
@@ -14,7 +19,13 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/practice" element={<Practice />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/" element={<Navigate to="/signin" replace />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
